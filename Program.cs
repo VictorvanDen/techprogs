03f4b960896c778b918090c3dfc2066edb64381a
VictorvanDen <qwe1234567890qweqwe@yandex.ru>
2020-11-09 11:38:25 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace z1
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int y, t, u;
+      y = Convert.ToInt32(Console.ReadLine());
+      t = Convert.ToInt32(Console.ReadLine());
+      u = t + y;
+      Console.WriteLine(Convert.ToString(u));
+      u = t * y;
+      Console.WriteLine(Convert.ToString(u));
+      u = y - t;
+      Console.WriteLine(Convert.ToString(u));
+      u = y / t;
+      Console.WriteLine(Convert.ToString(u));
+      u = y % t;
+      Console.WriteLine(Convert.ToString(u));
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
